5e6bd21626638c6e0b5de66dd8dd804ff18fc6b9
Marc Kalmes <mkalmes@mkalmes.net>
2020-09-20 16:19:44 +0200

    Remove Google Analytics

--- config.rb
+++ config.rb
@@ -16,12 +16,6 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-# General configuration
-
-activate :google_analytics do |ga|
-  ga.tracking_id = 'UA-42435342-1' # Replace with your property ID.
-end
-
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
